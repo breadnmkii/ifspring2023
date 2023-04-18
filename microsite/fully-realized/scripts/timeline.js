9ea8d34d65e55659cf09257bea2b71394bc85505
Breadn <bylueang@gmail.com>
2023-04-18 16:10:35 -0500

    more workkk

--- microsite/fully-realized/scripts/timeline.js
+++ microsite/fully-realized/scripts/timeline.js
@@ -4,8 +4,7 @@ let currentSlide = 0;
 // Add event listeners for left and right arrow buttons
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-prev.addEventListener('onClick', prevSlide);
-next.addEventListener('onClick', nextSlide);
+
 
 // Functions for setting next/prev slides to "active" class
 function nextSlide() {
